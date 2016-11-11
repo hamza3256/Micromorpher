33d46462dfefd067c0426c17601a3da78c3552a6
Steve Huckle <s.huckle@sussex.ac.uk>
2016-11-11 15:50:39 +0000

    Server start scripts

---
+++ src/scripts/doExchangeBuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR="/home/s/sh/sh601/gitrepos/ExchangeCurrency/src/react-do-exchange" 
+
+cd "$DIR" && npm run dev
---
+++ src/scripts/exchangeRatesBuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR="/home/s/sh/sh601/gitrepos/ExchangeCurrency/src/react-exchange-rates" 
+
+cd "$DIR" && npm run dev
