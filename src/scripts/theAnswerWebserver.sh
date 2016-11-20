f2055b574b62ecbb1f85588f9e469943f8f6604b
Steve Huckle <s.huckle@sussex.ac.uk>
2016-11-20 21:03:28 +0000

    Startup scripts

--- src/scripts/theAnswerWebserver.sh
+++ src/scripts/theAnswerWebserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="/share/ExchangeCurrency/src/TheAnswer"
+DIR="/share/ExchangeCurrency/src/react-the-answer"
 PATH=/opt/bitnami/redis/bin:/opt/bitnami/python/bin:/opt/bitnami/nodejs/bin:/opt/bitnami/git/bin:/opt/bitnami/apache2/bin:/opt/bitnami/common/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
 cd "$DIR" && npm run start
