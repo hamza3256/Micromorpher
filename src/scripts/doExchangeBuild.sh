f2474ec3e7754b3bde857afe62db50dd89445f94
Steve Huckle <s.huckle@sussex.ac.uk>
2016-11-21 09:33:43 +0000

    loop the dev builds

--- src/scripts/doExchangeBuild.sh
+++ src/scripts/doExchangeBuild.sh
@@ -3,4 +3,4 @@
 DIR="/share/ExchangeCurrency/src/react-do-exchange"
 PATH=/opt/bitnami/redis/bin:/opt/bitnami/python/bin:/opt/bitnami/nodejs/bin:/opt/bitnami/git/bin:/opt/bitnami/apache2/bin:/opt/bitnami/common/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-cd "$DIR" && npm run dev
+cd "$DIR" && while true; do npm run dev; sleep 10; done
