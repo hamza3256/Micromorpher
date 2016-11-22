b44ab9560965885d4306566337abd6bc22fcbc95
Steve Huckle <s.huckle@sussex.ac.uk>
2016-11-22 11:42:57 +0000

    Conf changes

--- src/scripts/doExchangeWebserver.sh
+++ src/scripts/doExchangeWebserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="/share/ExchangeCurrency/src/react-do-exchange"
-PATH=/opt/bitnami/redis/bin:/opt/bitnami/python/bin:/opt/bitnami/nodejs/bin:/opt/bitnami/git/bin:/opt/bitnami/apache2/bin:/opt/bitnami/common/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+DIR="/home/sussex/gitrepos/ExchangeCurrency/src/react-do-exchange"
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 cd "$DIR" && npm run start
