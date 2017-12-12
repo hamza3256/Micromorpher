a4d4c35a456ac9a05851559d1c0630e740c6617c
Steve Huckle <steve.huckle@gmail.com>
2017-12-12 15:16:14 +0000

    Webpack configs

---
+++ webpack.common.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+
+var config = {
+  entry: {
+    app: [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      './app/index.jsx'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new cleanWebpackPlugin(['build']),
+    new htmlWebpackPlugin({
+      template: './app/index.html',
+      inject: 'body',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true, // default is false
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.jsx?/,
+        include: path.resolve(__dirname, 'app'),
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
+
+module.exports = config
